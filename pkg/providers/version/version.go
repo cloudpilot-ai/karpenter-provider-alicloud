2d989d7d397b56b6284d8a03a0dac6dc5110b376
daimaxiaxie <codexiaxie@163.com>
2024-09-22 19:00:56 +0800

    impl VersionProvider

---
+++ pkg/providers/version/version.go
@@ -0,0 +1,90 @@
+package version
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/patrickmn/go-cache"
+	"github.com/samber/lo"
+	"k8s.io/apimachinery/pkg/util/version"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"sigs.k8s.io/karpenter/pkg/utils/pretty"
+)
+
+const (
+	kubernetesVersionCacheKey = "kubernetesVersion"
+	// Karpenter's supported version of Kubernetes
+	// If a user runs a karpenter image on a k8s version outside the min and max,
+	// One error message will be fired to notify
+	MinK8sVersion = "1.23"
+	MaxK8sVersion = "1.30"
+)
+
+type Provider interface {
+	Get(ctx context.Context) (string, error)
+}
+
+// DefaultProvider get the APIServer version. This will be initialized at start up and allows karpenter to have an understanding of the cluster version
+// for decision making. The version is cached to help reduce the amount of calls made to the API Server
+type DefaultProvider struct {
+	cache               *cache.Cache
+	cm                  *pretty.ChangeMonitor
+	kubernetesInterface kubernetes.Interface
+}
+
+func NewDefaultProvider(kubernetesInterface kubernetes.Interface, cache *cache.Cache) *DefaultProvider {
+	return &DefaultProvider{
+		cm:                  pretty.NewChangeMonitor(),
+		cache:               cache,
+		kubernetesInterface: kubernetesInterface,
+	}
+}
+
+func (p *DefaultProvider) Get(ctx context.Context) (string, error) {
+	if version, ok := p.cache.Get(kubernetesVersionCacheKey); ok {
+		return version.(string), nil
+	}
+	serverVersion, err := p.kubernetesInterface.Discovery().ServerVersion()
+	if err != nil {
+		return "", err
+	}
+	version := fmt.Sprintf("%s.%s", serverVersion.Major, strings.TrimSuffix(serverVersion.Minor, "+"))
+	p.cache.SetDefault(kubernetesVersionCacheKey, version)
+	if p.cm.HasChanged("kubernetes-version", version) {
+		log.FromContext(ctx).WithValues("version", version).V(1).Info("discovered kubernetes version")
+		if err := validateK8sVersion(version); err != nil {
+			log.FromContext(ctx).Error(err, "failed validating kubernetes version")
+		}
+	}
+	return version, nil
+}
+
+func validateK8sVersion(v string) error {
+	k8sVersion := version.MustParseGeneric(v)
+
+	// We will only error if the user is running karpenter on a k8s version,
+	// that is out of the range of the minK8sVersion and maxK8sVersion
+	if k8sVersion.LessThan(version.MustParseGeneric(MinK8sVersion)) ||
+		version.MustParseGeneric(MaxK8sVersion).LessThan(k8sVersion) {
+		return fmt.Errorf("karpenter version is not compatible with K8s version %s", k8sVersion)
+	}
+
+	return nil
+}
+
+// SupportedK8sVersions returns a slice of version strings in format "major.minor" for all versions of k8s supported by
+// this version of Karpenter.
+// Note: Assumes k8s only has a single major version (1.x)
+func SupportedK8sVersions() []string {
+	minMinor := lo.Must(strconv.Atoi(strings.Split(MinK8sVersion, ".")[1]))
+	maxMinor := lo.Must(strconv.Atoi(strings.Split(MaxK8sVersion, ".")[1]))
+	versions := make([]string, 0, maxMinor-minMinor+1)
+	for i := minMinor; i <= maxMinor; i++ {
+		versions = append(versions, fmt.Sprintf("1.%d", i))
+	}
+	return versions
+}
