d49bd5208afdf93870599045a3f34b6bcf604bf4
jwcesign <jiangwei@cloudpilot.ai>
2026-07-06 14:18:21 +0800

    fix x/net vulnerability
    Signed-off-by: jwcesign <jiangwei@cloudpilot.ai>

--- hack/toolchain.sh
+++ hack/toolchain.sh
@@ -11,8 +11,8 @@ main() {
 
 tools() {
     go install github.com/google/go-licenses@v1.6.0
-    go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.3
-    go install github.com/google/ko@v0.16.0
+    go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8
+    go install github.com/google/ko@v0.19.1
     go install github.com/mikefarah/yq/v4@v4.44.3
     go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.14.2
     go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
